6d42e682a64019f3027a0f1374974df4eb9491b2
Devraj Singh <aol.devraj@aol.com>
2025-02-24 17:46:18 +0530

    new file:   .gitignore (output.txt) 	modified:   main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 r =  requests.get('https://stackoverflow.com/questions', timeout=10).text
-
 def clear_links(links):
     parts = links.split('/')
     if len(parts) > 3:
@@ -11,8 +10,9 @@ def clear_links(links):
 soup = BeautifulSoup(r, 'html.parser')
 main_bar = soup.find("div", id="mainbar")
 links = main_bar.find_all("a", class_="s-link")
-for link in links:
-    href = link.get('href')
-    if href:
-        processed_text = clear_links(href)
-    print(processed_text)
+with open('stackoverflow_links.txt', 'w', encoding='utf-8') as file:
+	for link in links:
+		href = link.get('href')
+		if href:
+			processed_text = clear_links(href)
+		file.write(processed_text + '\n')
