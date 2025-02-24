6850d5fde2ebdeb1690d537b60a3dca3312a971b
Devraj Singh <aol.devraj@aol.com>
2025-02-24 17:26:58 +0530

    new file:   main.py simple python script to extract questions from stackoveflow homepage, made using requests and beautifulsoup4.

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+r =  requests.get('https://stackoverflow.com/questions', timeout=10).text
+
+def clear_links(links):
+    parts = links.split('/')
+    if len(parts) > 3:
+        left_text = '/'.join(parts[3:])
+        req_text = left_text.replace('-', ' ')
+        return req_text
+    return ""
+soup = BeautifulSoup(r, 'html.parser')
+main_bar = soup.find("div", id="mainbar")
+
+if main_bar:
+    links = main_bar.find_all("a", class_="s-link")
+    for link in links:
+        href = link.get('href')
+        if href:
+            processed_text = clear_links(href)
+            print(processed_text)
+else:
+    print("No 'mainbar' element found.")
