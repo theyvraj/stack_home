2bbbe886358daa14ede51c1382a1c28e49c16fff
Devraj Singh <aol.devraj@aol.com>
2025-02-24 17:56:16 +0530

    modified:   main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-r =  requests.get('https://stackoverflow.com/questions', timeout=10).text
+r = requests.get('https://stackoverflow.com/questions', timeout=10).text
+link_count = 0
 def clear_links(links):
+    global link_count
     parts = links.split('/')
     if len(parts) > 3:
         left_text = '/'.join(parts[3:])
         req_text = left_text.replace('-', ' ')
-        return req_text
+        link_count += 1
+        return f"{link_count}. {req_text}"
+    return ""
 soup = BeautifulSoup(r, 'html.parser')
 main_bar = soup.find("div", id="mainbar")
 links = main_bar.find_all("a", class_="s-link")
