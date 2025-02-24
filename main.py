04c7b45a86b809e147bc6ab1b0fb827b3094cdd2
Devraj Singh <aol.devraj@aol.com>
2025-02-24 18:54:37 +0530

    new file:   code.txt 	modified:   main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-r = requests.get('https://stackoverflow.com/questions', timeout=10).text
+base_url = 'https://stackoverflow.com/questions'
+r_ques = requests.get(base_url, timeout=10).text
 link_count = 0
 def clear_links(links):
     global link_count
@@ -11,12 +12,15 @@ def clear_links(links):
         link_count += 1
         return f"{link_count}. {req_text}"
     return ""
-soup = BeautifulSoup(r, 'html.parser')
-main_bar = soup.find("div", id="mainbar")
+soup_ques = BeautifulSoup(r_ques, 'html.parser')
+main_bar = soup_ques.find("div", id="mainbar")
 links = main_bar.find_all("a", class_="s-link")
 with open('stackoverflow_links.txt', 'w', encoding='utf-8') as file:
 	for link in links:
 		href = link.get('href')
 		if href:
 			processed_text = clear_links(href)
-		file.write(processed_text + '\n')
+r_ans = requests.get(base_url + href, timeout=10).text
+soop_ans = BeautifulSoup(r_ans, 'html.parser')
+print(soop_ans)
+item_prop = soop_ans.find_all("div", class_="inner-content clearfix")
