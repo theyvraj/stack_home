dbfd7bf824c9664107e1486569bc5ad5645808d4
Devraj Singh <aol.devraj@aol.com>
2025-02-25 12:09:28 +0530

    modified:   flipkart.py 	new file:   sample.html

--- flipkart.py
+++ flipkart.py
@@ -3,12 +3,13 @@ from bs4 import BeautifulSoup
 base_url = 'https://www.flipkart.com/search?q='
 product = str(input("Enter product name : "))
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'}
-full_url = base_url + product
+end = '&sort=relevance'
+full_url = base_url + product + end
 r = requests.get(full_url, headers=headers, timeout=10)
 r.raise_for_status()
 soup = BeautifulSoup(r.text, 'html.parser')
 links = set()
-for a_tag in soup.find_all('a', href=True):
+for a_tag in soup.find_all(class_=["WKTcLC BwBZTg", "WKTcLC"]):
     if "/p/" in a_tag["href"]:
         links.add("https://www.flipkart.com" + a_tag["href"])
 with open('flipkart_links.txt', 'w') as file:
